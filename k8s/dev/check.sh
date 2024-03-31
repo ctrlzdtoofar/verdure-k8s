e236b463f9ab41cb0a14ec973c1eadee85ad292f
Heather Stevens <github.implicit987@passmail.net>
2024-03-31 15:56:55 -0400

    logging fix for port forwarding

--- k8s/dev/check.sh
+++ k8s/dev/check.sh
@@ -21,7 +21,7 @@ kubectl logs job/db-init-job
 kubectl logs translator-deployment-5f547fb789-6zp68 --previous
 
 nohup kubectl port-forward service/postgres-cluster-ip-service 5432:5432 > port-forward.log 2>&1 &
-nohup kubectl port-forward service/tst-postgres-cluster-ip-service 5433:5433 > port-forward.log 2>&1 &
+nohup kubectl port-forward service/tst-postgres-cluster-ip-service 5433:5433 > tst-port-forward.log 2>&1 &
 
 # to turn off port forwarding:
 ps -ef | grep kubectl
